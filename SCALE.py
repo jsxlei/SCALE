cd116f247f99ef1fa0e05b5ed504a6a22df3b39c
XiongLei <jsxlei@gmail.com>
2019-05-24 16:17:20 +0800

    version 1.0.1

--- SCALE.py
+++ SCALE.py
@@ -19,38 +19,40 @@ import torch
 
 import numpy as np
 import pandas as pd
+import os
 import argparse
 
 from scale import SCALE
 from scale.dataset import SingleCellDataset
-from scale.utils import read_labels, cluster_report
-from scale import config
+from scale.utils import read_labels, cluster_report, estimate_k, binarization
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
 from torch.utils.data import DataLoader
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='SCALE: Single-Cell ATAC-seq Analysis via Latent feature Extraction')
-    parser.add_argument('--data', '-d', type=str, help='input data matrix peaks x samples')
+    parser.add_argument('--dataset', '-d', type=str, help='input dataset path')
     parser.add_argument('--n_centroids', '-k', type=int, help='cluster number')
-    parser.add_argument('--sep', type=str, default='\t', help='input data sep format \t or , ')
     parser.add_argument('--outdir', '-o', type=str, default='output/', help='Output path')
     parser.add_argument('--no_results', action='store_true', help='Not Save the results')
-    parser.add_argument('--verbose', action='store_false', help='Print loss of training process')
-    parser.add_argument('--reference', '-r', type=str, default=None, help='Whether ground truth available')
+    parser.add_argument('--binary', action='store_true', help='Transform imputed data to binary values')
+    parser.add_argument('--verbose', action='store_true', help='Print loss of training process')
     parser.add_argument('--pretrain', type=str, default=None, help='Load the trained model')
-    parser.add_argument('--epochs', '-e', type=int, default=None, help='Training epochs')
-    parser.add_argument('--lr', type=float, default=None, help='Learning rate')
-    parser.add_argument('--batch_size', '-b', type=int, default=None, help='Batch size')
+    parser.add_argument('--lr', type=float, default=0.002, help='Learning rate')
+    parser.add_argument('--batch_size', '-b', type=int, default=32, help='Batch size')
     parser.add_argument('--device', default='cuda', help='Use gpu when training')
     parser.add_argument('--seed', type=int, default=18, help='Random seed for repeat results')
-    parser.add_argument("--local_rank", type=int)
-#     parser.add_argument('--input_dim', type=int, default=None, help='Force input dim')
+    parser.add_argument("--beta", type=int, default=1)
+    parser.add_argument('-n', type=int, default=200)
+    parser.add_argument('--encode_dim', type=int, nargs='*', default=[1024, 128], help='encoder structure')
+    parser.add_argument('--decode_dim', type=int, nargs='*', default=[], help='encoder structure')
+    parser.add_argument('--latent', '-l',type=int, default=10, help='latent layer dim')
+    parser.add_argument('-x', type=int, default=0, help='Remove peaks (signal > 0) in less than X% of cells')
     parser.add_argument('--log_transform', action='store_true', help='Perform log2(x+1) transform')
-#     parser.add_argument('--gene_filter', action='store_true', help='Perform gene filter as SC3')
-#     parser.add_argument('-x', '--pct', type=float, default=6, help='Percent of genes when performing gene filter as SC3')
+    parser.add_argument('--max_iter', '-i', type=int, default=30000, help='Max iteration')
+    parser.add_argument('--weight_decay', type=float, default=5e-4)
 
     args = parser.parse_args()
 
@@ -61,93 +63,91 @@ if __name__ == '__main__':
 
     args.device = args.device if torch.cuda.is_available() and args.device!="cpu" else "cpu" 
     device = torch.device(args.device)
-    if args.batch_size is None:
-        batch_size = config.batch_size
-    else:
-        batch_size = args.batch_size
+    batch_size = args.batch_size
 
-    normalizer = MinMaxScaler()
-    dataset = SingleCellDataset(args.data, args.reference, transforms=[normalizer.fit_transform])
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    normalizer = MaxAbsScaler()
+    dataset = SingleCellDataset(args.dataset, X=args.x, transforms=[normalizer.fit_transform])
+    trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    testloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False)
     device = args.device
 
     cell_num = dataset.shape[0] 
     input_dim = dataset.shape[1] 	
-
-    k = args.n_centroids
-
-    if args.epochs is None:
-        epochs = config.epochs
+    
+    if args.n_centroids is None:
+        k = estimate_k(dataset.data.T)
+        print('Estimate k {}'.format(k))
     else:
-        epochs = args.epochs
-    if args.lr is None:
-        lr = config.lr
-    else:
-        lr = args.lr
-
+        k = args.n_centroids
+    lr = args.lr
+    name = args.dataset.strip('/').split('/')[-1]
+        
     print("\n**********************************************************************")
     print("  SCALE: Single-Cell ATAC-seq analysis via Latent feature Extraction")
     print("**********************************************************************\n")
     print("======== Parameters ========")
-    print('Cell number: {}\nInput_dim: {}\nn_centroids: {}\nEpoch: {}\nSeed: {}\nDevice: {}'.format(
-        cell_num, input_dim, k, epochs, args.seed, args.device))
+    print('Cell number: {}\nPeak number: {}\nn_centroids: {}\nmax_iter: {}\nbatch_size: {}'.format(
+        cell_num, input_dim, k, args.max_iter, batch_size))
     print("============================")
 
-    dims = [input_dim, config.latent, config.encode_dim, config.decode_dim]
+    dims = [input_dim, args.latent, args.encode_dim, args.decode_dim]
     model = SCALE(dims, n_centroids=k)
-    print(model)
-    # torch.distributed.init_process_group(backend='gloo', world_size=4, init_method='env://')
-    # model = torch.nn.parallel.DistributedDataParallelCPU(model)
 #     print(model)
 
     if not args.pretrain:
         print('\n## Training Model ##')
         t0 = time.time()
-        model.init_gmm_params(dataloader)
-        model.fit(dataloader,
+        model.init_gmm_params(testloader)
+        model.fit(trainloader,
                   lr=lr, 
-                  weight_decay=config.weight_decay, 
-                  epochs=epochs, 
+                  weight_decay=args.weight_decay,
                   verbose=args.verbose,
-                  print_interval=config.print_interval,
-                  device = device
+                  device = device,
+                  max_iter=args.max_iter,
+                  n=args.n,
+                  beta=args.beta,
+                  name=name
                    )
-        print('\nRunning Time: {:.2f} s'.format(time.time()-t0))
+        print('\nRunning Time: {:.2f} s'.format((time.time()-t0)/60))
     else:
         print('\n## Loading Model {} ##\n'.format(args.pretrain))
         model.load_model(args.pretrain)
-
-    # Clustering Report
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-    if args.reference:
-        pred = model.predict(dataloader, device)
-        cluster_report(dataset.labels, pred, dataset.CellType)
+        model.to(device)
 
     outdir = args.outdir
-    import os
     if not args.no_results:
+        print('outidr: {}'.format(outdir))
         if not os.path.exists(outdir):
             os.makedirs(outdir)
 
-        model.eval()
-        torch.save(model.state_dict(), os.path.join(outdir, 'model.pt')) # save model file
-
         ### output ###
         # 1. latent GMM feature
+        feature = model.encodeBatch(testloader, device=device, out='z')
+        pd.DataFrame(feature).to_csv(os.path.join(outdir, 'feature.txt'), sep='\t', header=False)
         
-        feature = model.encodeBatch(dataloader, device=device, out='z')
-
         # 2. cluster assignments
-        pred = model.predict(dataloader, device)
-
+        pred = model.predict(testloader, device)
+        pd.Series(pred).to_csv(os.path.join(outdir, 'cluster_assignments.txt'), sep='\t', header=False)
+                               
+        # gmm_pred = model.predict(testloader, device, method='gmm')
+        # pd.Series(gmm_pred).to_csv(os.path.join(outdir, 'gmm_predict.txt'), sep='\t', header=False)
+        
+        if dataset.celltype is not None:
+            y = dataset.CellType[dataset.celltype]
+        else:
+            y = pred
+            
         # 3. imputed data
-        recon_x = model.encodeBatch(dataloader, device, out='x', transforms=[normalizer.inverse_transform])
+        recon_x = model.encodeBatch(testloader, device, out='x', transforms=[normalizer.inverse_transform])
+        recon_x = pd.DataFrame(recon_x.T, index=dataset.peaks, columns=dataset.cell_id)
+        if args.binary:
+            recon_x = binarization(recon_x, raw)
+        recon_x.to_csv(os.path.join(outdir, 'imputed_data.txt'), sep='\t') 
 
-        assign_file = os.path.join(outdir, 'cluster_assignments.txt')
-        feature_file = os.path.join(outdir, 'feature.txt')
-        impute_file = os.path.join(outdir, 'imputed_data.txt')
+        torch.save(model.to('cpu').state_dict(), os.path.join(outdir, 'model.pt')) # save model
+        np.savetxt(os.path.join(args.outdir, 'mu_c.txt'), model.mu_c.cpu().detach().numpy())
 
-        pd.Series(pred).to_csv(assign_file, sep='\t', header=False) # save cluster assignments
-        pd.DataFrame(feature).to_csv(feature_file, sep='\t', header=False) # save latent feature
-        pd.DataFrame(recon_x.T, index=dataset.peaks, columns=dataset.sample_id).to_csv(impute_file, sep='\t') # save imputed data
-
+        from scale.plot import plot_embedding
+        plot_embedding(feature, y, #marker=model.mu_c.cpu().detach().numpy().T, 
+                       save=os.path.join(outdir, 'tsne.pdf'), save_emb=os.path.join(outdir, 'tsne.txt'))
+        
