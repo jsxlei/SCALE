16f7b064da78379972be7e00ee687e1c5ac5f792
jsxlei <jsxlei@gmail.com>
2019-06-13 09:29:06 +0800

    Update SCALE.py

--- SCALE.py
+++ SCALE.py
@@ -48,6 +48,7 @@ if __name__ == '__main__':
     parser.add_argument('--decode_dim', type=int, nargs='*', default=[], help='encoder structure')
     parser.add_argument('--latent', '-l',type=int, default=10, help='latent layer dim')
     parser.add_argument('-x', type=float, default=0, help='Remove peaks (signal > 0) in less than X percent of cells')
+    parser.add_argument('--min_peaks', type=int, default=0, help='Remove cells with less than min_peaks vailid peaks')
     parser.add_argument('--log_transform', action='store_true', help='Perform log2(x+1) transform')
     parser.add_argument('--max_iter', '-i', type=int, default=30000, help='Max iteration')
     parser.add_argument('--weight_decay', type=float, default=5e-4)
@@ -66,7 +67,7 @@ if __name__ == '__main__':
     batch_size = args.batch_size
 
     normalizer = MaxAbsScaler()
-    dataset = SingleCellDataset(args.dataset, X=args.x, transforms=[normalizer.fit_transform])
+    dataset = SingleCellDataset(args.dataset, X=args.x, min_peaks=args.min_peaks, transforms=[normalizer.fit_transform])
     trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     testloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False)
     device = args.device
